dd2b0b29640ef5375bcb1767d0d936d1abf8dcea
Lundovskikh Kirill <borzeman@gmail.com>
2023-03-28 15:03:16 +0500

    added post_create.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from .models import Post, Group, User
+from .forms import PostForm
 
 NUM_ART = 10  # Количество выводимых статей
-    
+
 
 def index(request):
     post_list = Post.objects.all()
@@ -53,3 +55,32 @@ def post_detail(request, post_id):
         'post': post,
     }
     return render(request, 'posts/post_detail.html', context)
+
+
+@login_required
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=request.user.username)
+    context = {'form': form, 'is_edit': False}
+    return render(request, 'posts/post_create.html', context)
+
+
+@login_required
+def post_edit(request, post_id):
+    is_edit = True
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+    form = PostForm(request.POST or None, isinstance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.save()
+        return redirect('posts:post_detail', pk=post_id)
+    context = {
+        'form': form,
+        'is_edit': is_edit,
+        'post': post
+    }
+    return render(request, 'posts/post_create.html', context)
